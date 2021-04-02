84bd32bdb0ef92e2a7a11a813d4e6cb77a180d06
Logan Coates <coates.logan1125@gmail.com>
2021-04-01 18:26:52 -0600

    cards

--- blueslides/src/components/Cards.js
+++ blueslides/src/components/Cards.js
@@ -5,15 +5,35 @@ import "./Cards.css";
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out these</h1>
+      <h1>Check out</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/img-9.jpg"
-              text="Hunting photos"
+              src="images/elk.jpeg"
+              text="Hunting, guiding photos and videos"
               label="Hunting"
               path="/hunting"
+            />
+            <CardItem
+              src="images/grandpa fish.jpg"
+              text="Fishing"
+              label="Fishing"
+              path="/fishing"
+            />
+          </ul>
+          <ul className="cards__items">
+          <CardItem
+              src="images/ammo.jpg"
+              text="Ammo"
+              label="Ammo"
+              path="/ammo"
+            />
+             <CardItem
+              src="images/3006.jpg"
+              text="Guns"
+              label="Guns"
+              path="/guns"
             />
           </ul>
         </div>
